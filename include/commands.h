210a33d033e6879066d82063a40198722c379bf5
Samuk <sammy.pico.duran@gmail.com>
2025-08-05 20:58:46 -0500

    [HotFix]: Missing signatures on header

--- include/commands.h
+++ include/commands.h
@@ -1,8 +1,23 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 // Declaraciones de funciones de comandos
 void comandoSalida();
 void comandoAyuda();
+bool valCargar(string command);
+bool valListar(string command);
+bool valHistograma(string command);
+bool valEsSubsecuencia(string command);
+bool valEnmascarar(string command);
+bool valGuardar(string command);
+bool valCodificar(string command);
+bool valDecodificar(string command);
+bool valRutaMasCorta(string command);
+bool valBaseRemota(string command);
 
 #endif // COMMANDS_H
