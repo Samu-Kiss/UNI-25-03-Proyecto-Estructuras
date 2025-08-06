5c68085475572517d85cc7abdf10647f1536071f
Sam <samypipe470@gmail.com>
2025-08-05 20:20:55 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include "./include/termcolor.hpp" // Librería para colores en la consola
+#include "../external/termcolor.hpp" // Librería para colores en la consola
+#include "../include/commands.h"     // Incluir comandos personalizados
 
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World"};
+int main() {
+    string command;
 
-    // Usar diferentes colores para cada palabra
-    cout << termcolor::red << msg[0] << " ";
-    cout << termcolor::green << msg[1] << " ";
-    cout << termcolor::blue << msg[2] << " ";
-    cout << termcolor::reset << endl;
+    cout << termcolor::green << "Consola Iniciada" << termcolor::reset << endl;
+    cout << "Escribe 'exit', 'quit' o 'salir' para terminar." << endl;
+    cout << "Si necesitas ayuda, escribe 'help' o 'ayuda'." << endl << endl;
+
+    while (true) {
+        // Mostrar el prompt $
+        cout << termcolor::yellow << "$ " << termcolor::reset;
+
+        // Leer el comando del usuario
+        getline(cin, command);
+
+        // Comandos de salida
+        if (command == "exit" || command == "quit" || command == "salir") {
+            comandoSalida();
+        }
+        // Comando de ayuda
+        else if (command == "help" || command == "ayuda") {
+            comandoAyuda();
+        }
+    }
+
+    return 0;
 }
