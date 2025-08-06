0301ac014774f7e19b4e062a6d2a9893775cdbbe
Sam <samypipe470@gmail.com>
2025-08-05 20:30:16 -0500

    Update commands.h

--- src/include/commands.h
+++ src/include/commands.h
@@ -4,5 +4,15 @@
 // Declaraciones de funciones de comandos
 void comandoSalida();
 void comandoAyuda();
+bool valCargar(string command);
+bool valListar(string command);
+bool valHistograma(string command);
+bool valEsSubsecuencia(string command);
+bool valEnmascarar(string command);
+bool valGuardar(string command);
+bool valCodificar(string command);
+bool valDecodificar(string command);
+bool valRutaMasCorta(string command);
+bool valBaseRemota(string command);
 
 #endif // COMMANDS_H
