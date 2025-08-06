3d768c222f36d91d00ed201aa48d6ab3649beb0c
Sam <samypipe470@gmail.com>
2025-08-05 20:27:16 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,52 @@ int main() {
         else if (command == "help" || command == "ayuda") {
             comandoAyuda();
         }
+         //validar comando cargar
+        else if (valCargar(command) != false) {
+            cout << termcolor::green << "comando válido: cargar" << termcolor::reset << endl;
+        }
+        //validar comando listar
+        else if (valListar(command) != false) {
+            cout << termcolor::green << "omando válido: listar_secuencias" << termcolor::reset << endl;
+        }
+        //validar comando histograma
+        else if (valHistograma(command) != false) {
+            cout << termcolor::green << "comando válido: histograma" << termcolor::reset << endl;
+        }
+        //validar comando subsecuencia
+        else if (valEsSubsecuencia(command) != false) {
+            cout << termcolor::green << "comando válido: es_subsecuencia" << termcolor::reset << endl;
+        }
+        //validar comando enmascarar
+        else if (valEnmascarar(command) != false) {
+            cout << termcolor::green << "comando válido: enmascarar" << termcolor::reset << endl;
+        }
+        //validar comando guardar
+        else if (valGuardar(command) != false) {
+            cout << termcolor::green << "comando válido: guardar" << termcolor::reset << endl;
+        }
+        //validar comando codificar
+        else if (valCodificar(command) != false) {
+            cout << termcolor::green << "comando válido: codificar" << termcolor::reset << endl;
+        }
+        //validar comando decodificar
+        else if (valDecodificar(command) != false) {
+            cout << termcolor::green << "comando válido: decodificar" << termcolor::reset << endl;
+        }
+        ////validar comando ruta_mas_corta
+        else if (valRutaMasCorta(command) != false) {
+            cout << termcolor::green << "comando válido: ruta_mas_corta" << termcolor::reset << endl;
+        }
+        //validar comando base_remota
+        else if (valBaseRemota(command) != false) {
+            cout << termcolor::green << "comando válido: base_remota" << termcolor::reset << endl;
+        }
+        //validar otros
+        else {
+            cout << termcolor::red << "Error: comando no reconocido o parámetros incorrectos." << termcolor::reset << endl;
+        }
+
+        cout << termcolor::cyan << "> Comando finalizado." << termcolor::reset << endl;
     }
 
     return 0;
