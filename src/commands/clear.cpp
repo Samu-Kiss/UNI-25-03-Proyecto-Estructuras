e35c08241d365ddd60c665668204708c681b5ad2
Samuk <sammy.pico.duran@gmail.com>
2025-08-08 13:11:39 -0500

    [Refactor]: Remove clear.cpp

--- src/commands/clear.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../../include/commands.h"
-#include "../../include/utils.h"
-#include "../../external/termcolor.hpp"
-
-using namespace std;
-
-// Este archivo contiene funciones relacionadas con el comando clear
-// La implementación de clearScreen() está en utils.cpp
