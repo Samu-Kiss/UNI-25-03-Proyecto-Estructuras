ba421bab35534c5f095690addf82b6af012d4f97
Sam <samypipe470@gmail.com>
2025-08-31 17:25:11 -0500

    Implementación Histograma

--- src/genoma.cpp
+++ src/genoma.cpp
@@ -35,6 +35,43 @@ void Genoma::Histograma(const char* descripcion_secuencia){
     // Posibles estados:
     // la secuencia no existe: secuencia inválida
     // la secuencia existe A: frecuencia a \n C: frecuencia c \n G: frecuencia g \n T: frecuencia t \n -: frecuencia -
+
+size_t tam = secuencias.size();
+    for (size_t i = 0; i < secuencias.size(); ++i) {
+        if (secuencias[i].descripcion == descripcion_secuencia) {
+            tam = i;
+            break;
+        }
+    }
+	
+    if (tam == secuencias.size()) { //No entro al if, no encontro la secuencia
+        cout << "Secuencia invalida" << endl;
+		return;  //Se sale
+    }
+
+    const char tabla[18] = {'A','C','G','T','U','R','Y','K','M','S','W','B','D','H','V','N','X','-'};
+    int frec[18];
+	
+    for (int i = 0; i < 18; ++i) {
+		frec[i] = 0;
+	}
+
+    const vector<char>& bases = secuencias[tam].bases;
+	
+    for (size_t i = 0; i < bases.size(); ++i) {
+        char b = bases[i];
+        for (int j = 0; j < 18; ++j) {
+            if (b == tabla[j]) { 
+				frec[j]++; 
+				break;
+				}
+        }
+    }
+
+    for (int i = 0; i < 18; ++i) {
+        cout << tabla[i] << " : " << frec[i] << endl;
+    }
+    
 }
 
 //EsSubsecuencia(subsecuencia) -> booleano
