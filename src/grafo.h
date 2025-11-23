95d95705b0ce3620a73109d93a78127f5e06b0e1
Sam <samypipe470@gmail.com>
2025-11-23 15:34:37 -0500

    TAD grafo

---
+++ src/grafo.h
@@ -0,0 +1,33 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+
+#include <vector>
+
+//Estructura para devolver los resultados de Dijkstra
+struct ResultadoDijkstra {
+    std::vector<double> distancias;
+    std::vector<int> previos;
+};
+
+class Grafo {
+private:
+    int numVertices;
+    // Matriz de Adyacencia matriz[origen][destino] = peso.
+    // Si no hay arista, el valor es INFINITO.
+    std::vector<std::vector<double> > matrizAdyacencia;
+
+    // Constante para representar infinito
+    const double INFINITO;
+
+public:
+    // Constructor Inicializa la matriz de adyacencia
+    Grafo(int vertices);
+
+    // Metodo para agregar una arista con peso
+    void agregarArista(int origen, int destino, double peso);
+
+    // Algoritmo de Dijkstra
+    ResultadoDijkstra dijkstra(int nodoOrigen);
+};
+
+#endif // GRAFO_H
